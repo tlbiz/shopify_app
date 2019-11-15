3ca28dc54fc17092308874b224264a057f399f85
Carol Chen <carol.chen@shopify.com>
2019-11-15 11:32:36 -0500

    Remove some comments

--- test/generators/hmac_controller_generator_test.rb
+++ test/generators/hmac_controller_generator_test.rb
@@ -5,17 +5,6 @@ class HmacControllerGeneratorTest < Rails::Generators::TestCase
   tests ShopifyApp::Generators::HmacControllerGenerator
   destination File.expand_path("../tmp", File.dirname(__FILE__))
 
-  # setup do
-  #   ShopifyApp.configure do |config|
-  #     config.embedded_app = true
-  #   end
-
-  #   prepare_destination
-  #   provide_existing_application_file
-  #   provide_existing_routes_file
-  #   provide_existing_application_controller
-  # end
-
   test "creates the hmac verification controller" do
     run_generator
     assert_file "app/controllers/hmac_verification_controller.rb"
